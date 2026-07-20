5da754331a242c10dd3fc8fe100d84170c4f2977
redsky0212 <wihyun0238@gmail.com>
2026-07-20 14:07:20 +0900

    가이드 메인 문구 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -200,7 +200,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} Redsky Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} NIC company Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
